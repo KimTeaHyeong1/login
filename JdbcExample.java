a08a63feb6e8ff55533f87eb32266e25663ada06
KimTeaHyeong1 <132879692+KimTeaHyeong1@users.noreply.github.com>
2024-01-30 11:19:33 +0900

    Add files via upload

---
+++ JdbcExample.java
@@ -0,0 +1,47 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class JdbcExample {
+
+    public static void main(String[] args) {
+        String jdbcUrl = "jdbc:mysql://localhost:3306/database_name";
+        String user = "your_username";
+        String password = "your_password";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        try (Connection connection = DriverManager.getConnection(jdbcUrl, user, password)) {
+            if (connection != null) {
+                System.out.println("데이터베이스 연결 성공");
+
+                // users 테이블 조회
+                String query = "SELECT * FROM users";
+                try (PreparedStatement preparedStatement = connection.prepareStatement(query);
+                     ResultSet resultSet = preparedStatement.executeQuery()) {
+
+                    // 결과 출력
+                    while (resultSet.next()) {
+                        int userId = resultSet.getInt("user_id");
+                        String name = resultSet.getString("name");
+
+                        System.out.println("User ID: " + userId + ", name " + name);
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                System.out.println("데이터베이스 연결 실패.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
